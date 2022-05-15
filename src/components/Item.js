ff7930ba45677e26c0767202f6d4dff1004956f0
Samuel Hinga <samuelhinga200@gmail.com>
2022-05-16 02:45:49 +0300

    Assigment complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inside, setInside] = useState(true)
+
+  function handleInside(){
+    setInside((turnt) => !turnt);
+  }
+
+  const running = inside ? "" : "in-cart"
+  const ruin = inside ? "add" : "remove"
   return (
-    <li className="">
+    <li className={running}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={ruin} onClick={handleInside}>{inside ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
